eaa4b57133b22622c7477e312dafb0827612b723
Adwait Magikar <amagika1@binghamton.edu>
2019-07-15 21:52:56 -0400

    Added a new class for new page and linked to new view controller

---
+++ TempConverter/Page2.swift
@@ -0,0 +1,13 @@
+//
+//  Page2.swift
+//  TempConverter
+//
+//  Created by Adwait Magikar on 7/15/19.
+//  Copyright © 2019 Adwait Magikar. All rights reserved.
+//
+
+import UIKit
+
+class Page2: NSObject {
+
+}
