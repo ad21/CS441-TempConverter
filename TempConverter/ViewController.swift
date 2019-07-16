37256207fd1d325c7759abe523dbd3c4512296f1
Adwait Magikar <amagika1@binghamton.edu>
2019-07-16 17:30:58 -0400

    Connected slider,labels to viewcontroller

--- TempConverter/ViewController.swift
+++ TempConverter/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBOutlet weak var Farhenheit: UILabel!
+    @IBOutlet weak var Celsius: UILabel!
+    
+    @IBOutlet weak var Slider: UISlider!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
